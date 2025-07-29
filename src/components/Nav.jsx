18e486891c33969a7099ae9b8db25c6cc9f746ae
Joshua Hill <jjhi11240@gmail.com>
2025-07-28 22:06:20 -0700

    Hero Modifications

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,15 @@
 import { NavLink, Link } from "react-router-dom";
-import "./Nav.css"; 
+import "./Nav.css";
 
 export default function Nav() {
   return (
     <nav className="navbar-container">
       <Link to="/" className="JHill"><div>JH</div></Link>
       <div className="navbar">
-        <NavLink to="/" className="nav-link" activeclasscame="active">Home</NavLink>
-        <NavLink to="/about" className="nav-link" activeclasscame="active">About</NavLink>
-        <NavLink to="/projects" className="nav-link" activeclasscame="active">Projects</NavLink>
-        <NavLink to="/blogs" className="nav-link" activeclasscame="active">Blogs</NavLink>
+        <NavLink to="/" className="nav-link" activeclasscame="active"><span class="hover-underline-animation"> Home </span></NavLink>
+        <NavLink to="/about" className="nav-link" activeclasscame="active"><span class="hover-underline-animation"> About </span></NavLink>
+        <NavLink to="/projects" className="nav-link" activeclasscame="active"><span class="hover-underline-animation"> Projects </span></NavLink>
+        <NavLink to="/blogs" className="nav-link" activeclasscame="active"><span class="hover-underline-animation"> Blogs </span></NavLink>
       </div>
     </nav>
   );
